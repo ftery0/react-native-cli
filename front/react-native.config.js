8ef6a1649bcee121fe30589431833dc887f77b91
ftery0 <lerb5253@dgsw.hs.kr>
2024-11-18 14:24:47 +0900

    fix :: error

--- front/react-native.config.js
+++ front/react-native.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    dependencies: {
-      'react-native-vector-icons': {
-        platforms: {
-          ios: null,
-        },
-      },
+  project: {
+    ios: {
+      sourceDir: './ios',
     },
-  };
+    android: {
+      sourceDir: './android',
+    },
+  },
+  dependencies: {},
+};
